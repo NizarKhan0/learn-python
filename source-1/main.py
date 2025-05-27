dbf641f200309c00bc3f8cdc9614cb5da19d8b89
Nizar Khan <nizarkhan7071@gmail.com>
2025-05-27 21:29:56 +0800

    numbers

---
+++ source-1/main.py
@@ -0,0 +1,38 @@
+#This is a comment
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
+
+# Fundamental Data Types
+# int and float
+
+print(type(6))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4)) #0.5
+
+print(type(20+1.1))
+print(2 ** 3)
+print(2 // 4)
+print(2 % 4)
+
+# math functions
+print(abs(-2)) # 2
+print(round(2.6)) # 3
+
+bool
+str
+list
+tuple
+set
+dict
+
+# Classes -> custom types
+
+# Specialized Data Types
+
+None
