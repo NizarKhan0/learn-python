7347d3b84a570eab7c3e7e5445a31e369857eb83
Nizar Khan <nizarkhan7071@gmail.com>
2025-06-01 02:38:53 +0800

    dictionaries

--- source-1/numbers.py
+++ source-1/numbers.py
@@ -1,4 +1,4 @@
-#This is a comment
+# This is a comment
 
 """
 This is a comment
@@ -13,16 +13,16 @@ print("Hello, World!")
 print(type(6))
 print(type(2 - 4))
 print(type(2 * 4))
-print(type(2 / 4)) #0.5
+print(type(2 / 4))  # 0.5
 
-print(type(20+1.1))
-print(2 ** 3)
+print(type(20 + 1.1))
+print(2**3)
 print(2 // 4)
 print(2 % 4)
 
 # math functions
-print(abs(-2)) # 2
-print(round(2.6)) # 3
+print(abs(-2))  # 2
+print(round(2.6))  # 3
 
 bool
 str
@@ -40,18 +40,18 @@ None
 
 # operator precedence
 
-print(2 + 3 * 4) # 14
-print((2 + 3) + 4 ** 4) # 81
+print(2 + 3 * 4)  # 14
+print((2 + 3) + 4**4)  # 81
 
 # ()
 # **
 
 # operational bin() and complex
 
-print(bin(10)) # 0b1010
-print(int('0b1010', 2)) # 10
+print(bin(10))  # 0b1010
+print(int("0b1010", 2))  # 10
 
-#Variables
+# Variables
 
 # Legal variable names:
 
@@ -65,7 +65,7 @@ print(int('0b1010', 2)) # 10
 user_iq = 190
 print(user_iq)
 
-a,b,c = 1, 2, 3
+a, b, c = 1, 2, 3
 print(a)
 print(b)
 print(c)
@@ -79,17 +79,17 @@ some_value -= 2  # This is the same as some_value = some_value - 2
 
 print(some_value)
 
-#strings
+# strings
 
 print(type("Hello Mimizu!"))
 
 username = "Mimizu"
-password = '1234'
-long_string = '''
+password = "1234"
+long_string = """
 WOW
 O O
 ---
-'''
+"""
 print(long_string)
 
 first_name = "Nizar"
@@ -99,13 +99,13 @@ print(full_name)
 
 
 # string concatenation
-print('Hello ' + 'World!')  # Hello World!
+print("Hello " + "World!")  # Hello World!
 
 # Type conversion
 
 a = str(123)  # '123'
 b = int(a)  # 123
-c = type(b) # <class 'int'>
+c = type(b)  # <class 'int'>
 print(c)  # <class 'int'>
 
 # print(type(str(123)))  # '123'
@@ -115,7 +115,7 @@ print(c)  # <class 'int'>
 
 # \n, \t, \\, \', \"
 
-weather = "It\'s a nice day \nwith a lot of sunshine"
+weather = "It's a nice day \nwith a lot of sunshine"
 print(weather)  # It's a nice day
 
 
@@ -124,20 +124,20 @@ print(weather)  # It's a nice day
 name = "Nizar"
 age = 24
 print(f"Hello, my name is {name} and I'm {age} years old.")
-print('Hi '+ name + '. You are ' + str(age) + ' years old.')
-print('Hi {}. You are {} years old.'.format(name, age))
+print("Hi " + name + ". You are " + str(age) + " years old.")
+print("Hi {}. You are {} years old.".format(name, age))
 
 # String index
 
-selfish = 'me me me'
-        #  01234567
+selfish = "me me me"
+#  01234567
 
-print(selfish[0]) # m
+print(selfish[0])  # m
 # [start:stop]
-print(selfish[0:2]) # me
+print(selfish[0:2])  # me
 # [start:stop:stepover]
-print(selfish[0:2:5]) # m
-print(selfish[-1]) # e start from the end
+print(selfish[0:2:5])  # m
+print(selfish[-1])  # e start from the end
 
 # Immutability
 
@@ -146,26 +146,26 @@ print(selfish)  # 100
 
 # Built in Functions -  Python string methods
 
-greet = 'Hello'
+greet = "Hello"
 print(len(greet))  # 5
-print(greet[0:len(greet)])  # Hello
-print(greet[0:len(greet):2])  # Hlo
+print(greet[0 : len(greet)])  # Hello
+print(greet[0 : len(greet) : 2])  # Hlo
 print(greet[::-1])  # olleH
 
 
-quote = 'to be or not to be'
+quote = "to be or not to be"
 
 print(quote.upper())  # TO BE OR NOT TO BE
 print(quote.lower())  # to be or not to be
 print(quote.title())  # To Be Or Not To Be
 print(quote.capitalize())  # To be or not to be
-print(quote.replace('be', 'me'))  # to me or not to me
-print(quote.find('be'))  # 3
-print(quote.count('be'))  # 2
+print(quote.replace("be", "me"))  # to me or not to me
+print(quote.find("be"))  # 3
+print(quote.count("be"))  # 2
 
 # booleans
 
-name = 'Nizar'
+name = "Nizar"
 is_cool = True
 print(f"Hi, my name is {name}. Am I cool? {is_cool}")
 print(bool(0))  # False
@@ -202,30 +202,26 @@ print(bool(1))  # True
 # List
 
 li = [1, 2, 3, 4, 5]
-li2 = ['a', 'b', 'c', 'd', 'e']
-li3 = [1, 2, 3, 'a', 'b', 'c', True]
+li2 = ["a", "b", "c", "d", "e"]
+li3 = [1, 2, 3, "a", "b", "c", True]
 
 print(li)
 
 # list slicing
-shopping_list = ['milk', 'bread', 'eggs', 'butter']
+shopping_list = ["milk", "bread", "eggs", "butter"]
 # print(shopping_list[0])  # milk
 # print(shopping_list[1:3])  # ['bread', 'eggs']
 
-shopping_list[0] = 'milk'
+shopping_list[0] = "milk"
 new_list = shopping_list[:]  # copy the entire list
 # new_list = shopping_list[0:3]  # ['bread', 'eggs', 'butter']
-shopping_list[0] = 'cheese'  # Change the first item to 'cheese'
+shopping_list[0] = "cheese"  # Change the first item to 'cheese'
 print(shopping_list)  # ['cheese', 'bread', 'eggs', 'butter']
 print(new_list)  # ['milk', 'bread', 'eggs']
 
 # Matrix
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 print(matrix[0][1])  # 2
 
 # list method 1
@@ -255,17 +251,17 @@ print(basket)  # []
 
 # list method 2
 
-basket = ['a', 'b', 'c', 'd', 'e', 'a']
+basket = ["a", "b", "c", "d", "e", "a"]
 
-print(basket.index('b'))
-print('a' in basket)  # True
-print('f' in basket)  # False
+print(basket.index("b"))
+print("a" in basket)  # True
+print("f" in basket)  # False
 
-print(basket.count('a'))  # 2
+print(basket.count("a"))  # 2
 
 # list method 3
 
-basket = ['a', 'x', 'b', 'c', 'd', 'e']
+basket = ["a", "x", "b", "c", "d", "e"]
 
 basket.sort()  # Sort the list in ascending order
 print(basket)  # ['a', 'b', 'c', 'd', 'e']
@@ -289,11 +285,15 @@ print(list(range(1, 10)))  # Create a range object from 1 to 9
 
 # Join
 
-sentence = '!'
-new_sentence = sentence.join(['This', 'is', 'a', 'sentence.'])  # Join the list of words into a sentence
+sentence = "!"
+new_sentence = sentence.join(
+    ["This", "is", "a", "sentence."]
+)  # Join the list of words into a sentence
 print(new_sentence)  # This is a new_sentence.
 
-sentence = ' '.join(['This', 'is', 'a', 'sentence.'])  # Join the list of words into a sentence
+sentence = " ".join(
+    ["This", "is", "a", "sentence."]
+)  # Join the list of words into a sentence
 print(sentence)  # This is a sentence.
 
 # list unpacking
@@ -309,3 +309,69 @@ print(d)  # 9
 
 weapons = None  # Assign None to a variable
 print(weapons)  # None
+
+
+# Dictionaries
+
+dictionary = {
+    "name": "Nizar",
+    "age": 24,
+    "is_cool": True,
+    "languages": ["Python", "JavaScript", "PHP"],
+}
+print(dictionary)  # {'name': 'Nizar', 'age': 24, 'is_cool': True}
+
+my_list = [
+    {
+        "name": "Nizar",
+        "age": 24,
+        "is_cool": True,
+        "languages": ["Python", "JavaScript", "PHP"],
+    },
+    {
+        "name": "Mimizu",
+        "age": 26,
+        "is_cool": True,
+        "cosplayer": ["Anime", "Manga", "Game"],
+    },
+]
+# Accessing values in a dictionary
+print(dictionary["name"])  # Nizar
+print(dictionary["languages"][2])  # PHP
+
+# Dictionary key
+
+print(dictionary.get("name"))  # Nizar
+print(
+    dictionary.get("address", "Not Found")
+)  # Not Found (default value if key doesn't exist)
+
+
+# Dictionary method 1
+
+user = {
+    "basket": [1, 2, 3, 4, 5],
+    "greet": "Hello",
+}
+
+print(user.keys())  # dict_keys(['basket', 'greet'])
+print(user.values())  # dict_values([[1, 2, 3, 4, 5], 'Hello'])
+
+user2 = dict(name="Nizar")
+print(user2)  # {'name': 'Nizar'}
+
+# Dictionary method 2
+
+print(user.items())  # dict_items([('basket', [1, 2, 3, 4, 5]), ('greet', 'Hello')])
+print(user.clear())  # Clear the dictionary
+print(user)  # {}
+
+print(user2.copy())  # Create a shallow copy of the dictionary
+print(user2)  # {'name': 'Nizar'}
+
+# print(user.pop("greet"))  # Remove and return the last item from the dictionary
+# print(user.popitem())  # Remove and return the last item from the dictionary
+# print(user)
+
+user2.update({"age": 24})  # Update the dictionary with a new key-value pair
+print(user2)  # {'name': 'Nizar', 'age': 24}
