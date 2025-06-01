9dfb17a7457563970cfb19d04e74d3734894e580
Nizar Khan <nizarkhan7071@gmail.com>
2025-06-01 19:44:11 +0800

    truthy & falsy

--- source-1/basic-ii.py
+++ source-1/basic-ii.py
@@ -9,3 +9,20 @@ elif is_old and not is_licensed:
     print("You are old enough to drive but you do not have a license.")
 else:
     print("You are not old enough to drive and you do not have a license.")
+
+
+# Truthy & Falsy
+
+is_old = bool(0)  # False
+is_licensed = bool("test")  # True
+
+print(is_old)  # False
+print(is_licensed)  # True
+
+username = "admin"
+password = ""
+
+if username and password:
+    print("{username} is logged in!.".format(username=username))
+elif username and not password:
+    print("Please enter your username or password!")
