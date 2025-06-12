a4033f781da48970d1f18cc7610ac1daf29a4148
Nizar Khan <nizarkhan7071@gmail.com>
2025-06-12 23:04:16 +0800

    attribute & method

--- source-1/oop.py
+++ source-1/oop.py
@@ -12,16 +12,31 @@
 
 # create class
 class PlayerCharacters:
+    # class object attribute
+    membership = True
     def __init__(self, name, age):
-        self.name = name  #attribute
+        # if (PlayerCharacters.membership):
+        self.name = name  # attribute
         self.age = age
-    def run(self):
-        # print(f'{self.name} is running')
-        print('run')
-        return 'done'
+
+    def shout(self):
+        print(f'{self.name} is shouting!')
+    
+    def run(self, hello):
+        print(f'{self.name} is running {hello}')
+
 
 player1 = PlayerCharacters('Nizar', 24)
 player2 = PlayerCharacters('John', 28)
 
-print(player1.age)
-print(player2.run())
+# Corrected code snippet:
+print(player1.age)        # 24
+player1.run('in the park')  # Nizar is running in the park (no None)
+player2.shout()           # John is shouting! (no None)
+print(player2.membership) # True
+
+# help(player1)  # help function to see the attributes and methods of the class
+# help(list)  # help function to see the attributes and methods of the list class
+
+
+
