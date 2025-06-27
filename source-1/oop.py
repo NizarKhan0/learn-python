29d930e2d0c2f3dd5f6173e14ca2d0cd0a12ccd0
Nizar Khan <nizarkhan7071@gmail.com>
2025-06-27 22:39:18 +0800

    MRO - Method Resolution Order

--- source-1/oop.py
+++ source-1/oop.py
@@ -259,4 +259,32 @@ class Hybrid(Wizardin, Archerin):
 
 hybrid1 = Hybrid('Merlin', 50, 100)
 print(hybrid1.attack())
-        
+
+
+# MRO - Method Resolution Order
+
+class A:
+    num = 10
+
+class B(A):
+    pass
+
+class C(A):
+    num = 1
+
+class D(B, C):
+    pass
+
+print(D.mro())
+# D.num
+class X:pass
+class Y:pass
+class Z:pass
+class A(X,Y):
+    pass
+class B(Y,Z):
+    pass
+class M(B,A,Z):
+    pass
+
+print(M.__mro__)
