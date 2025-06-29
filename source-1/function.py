79cc11a84a616711caae29eea7829cf8cb50bad4
Nizar Khan <nizarkhan7071@gmail.com>
2025-06-29 23:19:17 +0800

    fn - map()

--- source-1/function.py
+++ source-1/function.py
@@ -1,9 +1,18 @@
 # Pure function
 
-def multiply_by2(li):
-    new_list = []
-    for item in li:
-        new_list.append(item * 2)
-    return new_list
+# def multiply_by2(li):
+#     new_list = []
+#     for item in li:
+#         new_list.append(item * 2)
+#     return new_list
 
-print(multiply_by2([1, 2, 3]))
+# print(multiply_by2([1, 2, 3]))
+
+#  Mapping function
+
+my_list = [1, 2, 3]
+def multiply_by2(item):
+    return item * 2
+
+print(list(map(multiply_by2, my_list)))
+print(my_list)
