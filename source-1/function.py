882e4a8f49fa8df1e1b91eb60ceea1e52b08565f
Nizar Khan <nizarkhan7071@gmail.com>
2025-07-01 22:03:59 +0800

    fn - reduce()

--- source-1/function.py
+++ source-1/function.py
@@ -35,3 +35,17 @@ their_list = [100, 200, 300]
 
 print(list(zip(my_list3, you_list, their_list)))
 print(my_list3)
+
+
+# Reduce Function
+
+from functools import reduce
+
+my_list4 = [1, 2, 3]
+
+def accumulator(acc, item):
+    print(acc, item)
+    return acc + item
+
+print(reduce(accumulator, my_list4, 0))
+print(my_list4)
