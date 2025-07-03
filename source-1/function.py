145c2c2de94810e7b38277c9b0a9c3f8c7f8a455
Nizar Khan <nizarkhan7071@gmail.com>
2025-07-03 19:37:43 +0800

    fn - list comprehensions

--- source-1/function.py
+++ source-1/function.py
@@ -67,3 +67,50 @@ print(list(map(lambda item: item * 2, my_list6)))
 # List Sorting
 a = [(0, 2), (4, 3), (9, 9), (10, -1)]
 print(sorted(a, key=lambda item: item[1]))
+
+# List Comprehensions (list, set, dictionary)
+
+# list
+my_list7 = [char for char in 'hello']
+my_list8 = [num for  num in range(0, 100)]
+my_list9 = [num**2 for num in range(0, 100)]
+my_list10 = [num**2 for num in range(0, 100) if num % 2 == 0]
+
+print(my_list7)
+print(my_list8)
+print(my_list9)
+print(my_list10)
+
+# set
+
+simple_dict = {
+    'a': 1,
+    'b': 2,
+    'c': 3
+}
+
+# my_dict = {key:value**2 for key, value in enumerate(range(0, 100)) if value % 2 == 0}
+my_dict = {key: value**2 for key, value in simple_dict.items() if value % 2 == 0}
+
+my_dict2 = {num: num*2 for num in [1, 2, 3]}
+
+print(my_dict)
+print(my_dict2)
+
+
+# exercise
+
+# standart method
+some_list = ['a', 'b', 'c', 'd', 'b', 'm', 'n', 'n']
+
+duplicates = []
+for item in some_list:
+    if some_list.count(item) > 1 and item not in duplicates:
+        duplicates.append(item)
+
+print(duplicates)
+
+# list comprehension method
+
+duplicates2 = list(set([x for x in some_list if some_list.count(x) > 1]))
+print(duplicates2)
