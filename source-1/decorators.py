02f25cd9c255dd4f88d8f3612be75e300fd7fcd8
Nizar Khan <nizarkhan7071@gmail.com>
2025-07-08 23:53:16 +0800

    decorators 3

--- source-1/decorators.py
+++ source-1/decorators.py
@@ -52,3 +52,17 @@ bye()
 # a()
 
 # my_decorator(bye)()
+
+
+# decorators 3
+
+def my_decorator2(func):
+    def wrap_func(*args, **kwargs):
+        func(*args, **kwargs)
+    return wrap_func
+
+@my_decorator
+def hello2(greeting, emoji=":)"):
+    print(greeting, emoji)
+    
+hello2("Hello")
