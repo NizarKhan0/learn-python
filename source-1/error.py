4c990642ae03703bba21376fa56f9d4ee6fad2d2
Nizar Khan <nizarkhan7071@gmail.com>
2025-07-10 23:43:28 +0800

    errors

---
+++ source-1/error.py
@@ -0,0 +1,17 @@
+print(1+True)
+
+Exception error
+print(1+'hello')
+
+# Syntax error
+def test():
+    1 + name
+
+    li = [1, 2, 3]
+    print(li[3])
+
+    di = {'a': 1, 'b': 2}
+    print(di['c'])  # KeyError
+    print(1 / 0)
+    
+test()
