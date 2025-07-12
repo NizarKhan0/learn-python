7ab19844b2ec7dfda0dd9e38391d68a1696ff446
Nizar Khan <nizarkhan7071@gmail.com>
2025-07-12 23:37:37 +0800

    Error Handling 2

--- source-1/error.py
+++ source-1/error.py
@@ -19,15 +19,48 @@
 
 #  Error Handling
 
+# while True:
+#     try:
+#         age = int(input("What is your age? "))
+#         # print(f"Your age is: {age}")
+#         10/age
+#     except ValueError:
+#         print("PLease enter a number")
+#     except ZeroDivisionError:
+#         print("Please enter age higher than 0")
+#     else:
+#         print("Thank you!")
+#         break
+
+
+#  Error Handling 2
+
+def sum(num1, num2):
+    try:
+        return num1 + num2
+    except (TypeError, ZeroDivisionError) as err:
+    # except TypeError as err:
+        print(err)
+        # print(f'Please enter a numbers {err}')
+        # print(f'Please enter a number' + str(err))
+
+print(sum('1', 2))  # This will raise a TypeError
+# print(sum(1, 2))    # This will return 3
+
+# Excerise Error Handling
 while True:
     try:
         age = int(input("What is your age? "))
-        # print(f"Your age is: {age}")
         10/age
     except ValueError:
-        print("PLease enter a number")
+        print("Please enter a valid number")
+        continue
     except ZeroDivisionError:
-        print("Please enter age higher than 0")
+        print("Please enter an age higher than 0")
+        break
     else:
         print("Thank you!")
         break
+    finally:
+        print('ok, we are done!')
+    print('This will always run')
