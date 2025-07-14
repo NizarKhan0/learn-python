20681d1a06eddde57845ac532bfd58713c480ee0
Nizar Khan <nizarkhan7071@gmail.com>
2025-07-14 22:57:10 +0800

    Generators 2

--- source-1/generators.py
+++ source-1/generators.py
@@ -1,12 +1,28 @@
 # range(100)
 # list(range(100)
-     
-def make_list(num):
-    result = []
-    for i in range(num):
-        result.append(i)
-    return result
-    
-my_list = make_list(100)
+
+# def make_list(num):
+#     result = []
+#     for i in range(num):
+#         result.append(i)
+#     return result
+
+# my_list = make_list(100)
 # print(my_list)
-print(list(range(1000)))
+# print(list(range(1000)))
+
+# Generators 2
+# Iterable
+# Iterate
+
+def generator_function(num):
+    for i in range(num):
+        yield i * 2  # Yielding even numbers
+
+g = generator_function(100)
+next(g)  # This will get the first even number, which is 0
+next(g)  # This will get the second even number, which is 2
+print(next(g))  # This will print the first even number, which is 0
+# print(g)  # This will print a generator object
+# for item in generator_function(1000):
+#     print(item)
