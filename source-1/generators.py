d4d500c434215a7a1748040b8279d78159be1ef8
Nizar Khan <nizarkhan7071@gmail.com>
2025-07-15 19:02:55 +0800

    generators perfomance

--- source-1/generators.py
+++ source-1/generators.py
@@ -26,3 +26,31 @@ print(next(g))  # This will print the first even number, which is 0
 # print(g)  # This will print a generator object
 # for item in generator_function(1000):
 #     print(item)
+
+
+# generators perfomance
+
+from time import time
+def perfomance(fn):
+
+@perfomance
+def long_time():
+    print('1')
+    for i in range(1000000):
+        i * 5
+
+@perfomance
+def long_time2():
+    print('2')
+    for i in range(1000000):
+        i * 5
+
+long_time()
+long_time2()
+
+def gen_fun(num):
+    for i in range(num):
+        yield i
+
+for item in gen_fun(100):
+    print(item)
