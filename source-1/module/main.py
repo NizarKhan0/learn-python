ce3cb95eb5fe8320603597ead8b86a860f01da03
Nizar Khan <nizarkhan7071@gmail.com>
2025-07-19 23:10:28 +0800

    Modules - Packages

--- source-1/module/main.py
+++ source-1/module/main.py
@@ -1,4 +1,7 @@
 import utility
+import shopping.shopping_cart
 
 print(utility.multiply(3, 4))  # Output: 12
 print(utility.divide(10, 2))  # Output: 5.0
+
+print(shopping.shopping_cart.buy("apple"))  # Output: ['apple']
