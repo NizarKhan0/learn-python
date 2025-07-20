98dfafd64ca210f87ca0b1d0b20fc680f03a17f3
Nizar Khan <nizarkhan7071@gmail.com>
2025-07-20 22:43:20 +0800

    Modules - ways to import

--- source-1/module/main.py
+++ source-1/module/main.py
@@ -1,7 +1,18 @@
-import utility
-import shopping.shopping_cart
+# import utility
+from utility import multiply, divide
+# from shopping.more_shopping.shopping_cart import buy
+from shopping.more_shopping import shopping_cart
 
-print(utility.multiply(3, 4))  # Output: 12
-print(utility.divide(10, 2))  # Output: 5.0
+# print(utility.multiply(3, 4))  # Output: 12
+# print(utility.divide(10, 2))  # Output: 5.0
 
-print(shopping.shopping_cart.buy("apple"))  # Output: ['apple']
+# print(shopping.shopping_cart.buy("apple"))  # Output: ['apple']
+
+# print(buy("apple"))
+# Using the imported functions
+print(shopping_cart.buy("apple"))  # Output: ['apple']
+
+print(divide(10, 2))  # Output: 2.5
+print(multiply(3, 4))  # Output: 12
+
+# print(max[1, 2, 3])
--- source-1/module/utility.py
+++ source-1/module/utility.py
@@ -5,3 +5,6 @@ def multiply(num1, num2):
 def divide(num1, num2):
     """Divides num1 by num2 and returns the result. Raises ValueError if num2 is zero."""
     return num1 / num2
+
+def max():
+    return 'oops'
