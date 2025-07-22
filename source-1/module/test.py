e72d7f0c707915fb219bc5becb95f70ed2730ad4
Nizar Khan <nizarkhan7071@gmail.com>
2025-07-22 23:58:15 +0800

    Modules - Built in 2

--- source-1/module/test.py
+++ source-1/module/test.py
@@ -1,14 +1,19 @@
-import random
+# import random
 # import random as oulala
+import sys
 
 # print(dir(random))
-print(random.randint(1, 10))
-print(random.choice(['apple', 'banana', 'cherry']))
+# print(random.randint(1, 10))
+# print(random.choice(['apple', 'banana', 'cherry']))
 
-my_list = [1, 2, 3, 4, 5]
-random.shuffle(my_list)
-print(my_list)
+# my_list = [1, 2, 3, 4, 5]
+# random.shuffle(my_list)
+# print(my_list)
 
 # my_list = [1, 2, 3, 4, 5]
 # oulala.shuffle(my_list)
 # print(my_list)
+
+first = sys.argv[0]
+last = sys.argv[1]
+print(f"First argument: {first} Last argument: {last}")
