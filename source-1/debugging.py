aa302e01221250ca14b21dd7e74665fa6fa48de3
Nizar Khan <nizarkhan7071@gmail.com>
2025-07-27 23:08:32 +0800

    debugging

---
+++ source-1/debugging.py
@@ -0,0 +1,18 @@
+# debugging
+
+
+# linting
+# ide / editor
+# read error messages
+# pdb - python debugger
+
+import pdb
+
+def add(num1, num2):
+    # print(num1, num2)
+    # pdb.set_trace()  # Set a breakpoint here
+    # t = 4*5
+    return num1 + num2
+
+# add(4, 'string')
+print(add(4, 5))
