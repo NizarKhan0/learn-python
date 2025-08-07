611d7b787e511c28db2dbdaa0ed8c1f00ab416ec
Nizar Khan <nizarkhan7071@gmail.com>
2025-08-07 22:48:06 +0800

    Test - Unit Test

---
+++ source-1/testing/test.py
@@ -0,0 +1,21 @@
+# pyint
+# pyflake8
+
+# PEP 8
+
+import unittest
+import main
+
+class TestMain(unittest.TestCase):
+    def test_do_stuff(self):
+        test_param = 10
+        result = main.do_stuff(test_param)
+        self.assertEqual(result, 15)
+        
+    def test_do_stuff2(self):
+        test_param = 'asdasdsa'
+        result = main.do_stuff(test_param)
+        self.assertTrue(isinstance(result, ValueError))
+
+unittest.main()
+# This code is a unit test for the do_stuff function in the main module.
